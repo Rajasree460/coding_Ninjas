9dc0812e87b5e1ef715f6cfad10a06878ad5f4e4
Rajasree Laha <laharajasree121@gmail.com>
2024-11-28 09:48:15 +0530

    Create 1 to N Without Loop.java

---
+++ 1 to N Without Loop.java
@@ -0,0 +1,9 @@
+public class Solution {
+    public static int[] printNos(int x) {
+        int arr[]=new int[x];
+       for (int i=0;i<x;i++){
+           arr[i]=i+1;
+       }
+       return arr;
+    }
+}
