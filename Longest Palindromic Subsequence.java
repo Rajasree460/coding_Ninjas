b835adddfd60a5e6584beefc73e2b9d40518bb40
Rajasree Laha <laharajasree121@gmail.com>
2024-12-10 11:26:48 +0530

    Create  Longest Palindromic Subsequence.java

---
+++ Longest Palindromic Subsequence.java
@@ -0,0 +1,43 @@
+import java.util.* ;
+
+import java.io.*; 
+
+public class Solution {
+
+    public static int longestPalindromeSubsequence(String s) {
+
+        int i, j, n = s.length(), ans = 1;
+
+        int[] curr, prev = new int[n+1];
+
+        for(i=1; i<=n; i++){
+
+            curr = new int[n+1];
+
+            for(j=i; j>0; j--){
+
+                if (i == j)
+
+                    curr[j] = 1;
+
+                else if (s.charAt(i-1)==s.charAt(j-1))
+
+                    curr[j] = prev[j+1]+2;
+
+                else
+
+                    curr[j] = Math.max(curr[j+1], prev[j]);
+
+                ans = Math.max(ans, curr[j]);
+
+            }
+
+            prev = curr;
+
+        }
+
+        return ans;
+
+    }
+
+}
